82c6d55d67d8a1523bc298ef6837230dc514fdb8
varun <varunreddy@localhost>
2021-04-22 22:56:56 -0500

    Added Databases Interfaces

---
+++ Food/src/com/food/database/DbConnect.java
@@ -0,0 +1,10 @@
+package com.food.database;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public interface DbConnect {
+	public void getConnection();
+	public HashMap<String,LinkedList<String>> getOrderDetails();
+	public void chooseFoodItems();
+}
---
+++ Food/src/com/food/database/DbCustomer.java
@@ -0,0 +1,6 @@
+package com.food.database;
+
+public interface DbCustomer extends DbConnect{
+	public void chooseFoodItems();
+	public void confirmOrder();
+}
