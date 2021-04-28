af3770ee927d5a2247fb0f283e3a0dbaaf02b5fc
AjayTeja Reddy <ajaytejareddy@gmail.com>
2021-04-28 01:39:21 -0500

    login for customer, employee and manager is created

--- Food/src/app/Run.java
+++ Food/src/app/Run.java
@@ -1,9 +1,58 @@
 package app;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import com.food.modules.*;
 
 public class Run {
+	
+	static BufferedReader keyboard;
+    static Connection conn; 
+    static Statement stmt;
+	
+	private static void dbConnect() {
+		
+		//database user name and password
+    	String username="ard129", password = "CS470_6609";
+
+    	try { 
+
+     	   DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+
+                 System.out.println("Registered the driver...");
+
+     	    conn = DriverManager.getConnection (
+                        "jdbc:oracle:thin:@oracle1.wiu.edu:1521/toolman.wiu.edu",
+     		    username, password);
+
+     	    conn.setAutoCommit(false);
+     	    
+     	   stmt = conn.createStatement();
+    	}
+    	
+    	catch(SQLException e)
+	    {
+    		System.out.println("Error :100x1");
+    		//System.out.println("Caught SQL Exception: \n     " + e);
+    		System.exit(0);
+	    }
+		return ;
+
+	}
+	
+	
+	
 	public static void main(String[] args) throws IOException {
+		
+		dbConnect();
+		
+		@SuppressWarnings("unused")
+		LogIn user = new LogIn(conn);
+		
 		
 	}
 	
--- Food/src/com/food/modules/LogIn.java
+++ Food/src/com/food/modules/LogIn.java
@@ -3,12 +3,22 @@ package com.food.modules;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class LogIn {
 	
-	private String email,password,name;
-	private boolean isSignedIn;
+	@SuppressWarnings("unused")
+	private String email,password,name,address;
+	private boolean isSignedIn=false, isEmployee = false, isManager = false;
 	private BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	
+	private Connection conn; 
+    private Statement stmt;
+    private PreparedStatement ps;
 	
 	//instance initializer
 	{
@@ -16,20 +26,30 @@ public class LogIn {
 		isSignedIn = false;
 	}
 	
-	public LogIn() throws IOException {
+	public LogIn(Connection conn) throws IOException{
+		this.conn = conn;
+		if(isSignedIn == false) {
+			userInput();
+		}
+	}
+	
+	private void userInput() throws IOException{
 		System.out.println("Choose any Option:");
 		System.out.println("1.SignIn");
 		System.out.println("2.SignUp");
 		String option = input.readLine();
 		
-		while (option!="1"||option!="2")
+		//System.out.println((option.equals("1"))+"");
+		
+		while (!(option.equals("1") ^ option.equals("2")))
 			option = input.readLine();
 		
-		if (option == "1")
+		if (option.equals("1"))
 			signIn();
-		else
+		else if(option.equals("2"))
 			signUp();
 	}
+	
 	
 	
 	
@@ -38,21 +58,173 @@ public class LogIn {
 		System.out.printf("\nEnter Email: ");
 		email = input.readLine().toLowerCase();
 		
-		System.out.println("given name = "+email+" and password = "+password);
+		//System.out.println("given name = "+email+" and password = "+password);
 		//database logic for signing
-
+		
+		if(checkCustUser(email) == 1) {
+			//System.out.println("user login");
+			userLogin(email);
+		}
+		
+		else if(checkEmpUser(email) == 1) {
+			//System.out.format("%32s%16s\n","","Employee LoggedIn");
+			isEmployee = true;
+			empLogin(email);
+		}
+		else {
+			System.out.println("**User not EXIST**");
+		}
+		
+		
 	}
 	
+	private void empLogin(String email) {
+		String query = "SELECT name,isManager FROM Employees WHERE email=?";
+		ResultSet rsEmp=null;
+		
+		
+		try {
+			//setting prepared statement object and set parameters
+			ps=conn.prepareStatement(query);
+			ps.setString(1, email);
+			
+			//execute query
+			rsEmp = ps.executeQuery();
+			rsEmp.next();
+			
+			name = rsEmp.getString("name");
+			isManager = rsEmp.getString("isManager").equals("Y");
+			System.out.format("%32s%16s\n","","Employee LoggedIn");
+			System.out.println("Name = "+name+"\temail = "+email+" \tisManager?="+ isManager);
+			
+		}
+		catch(SQLException e) {
+			e.getMessage();
+			System.out.println("Database ERROR");
+		}
+		return ;
+	}
+
+	private void userLogin(String email) {
+		String query = "SELECT name,password,address FROM Customers WHERE email=?";
+		ResultSet rsCust=null;
+		
+		
+		try {
+			//setting prepared statement object and set parameters
+			ps=conn.prepareStatement(query);
+			ps.setString(1, email);
+			
+			//execute query
+			rsCust = ps.executeQuery();
+			rsCust.next();
+			
+			name = rsCust.getString("name");
+			password = rsCust.getNString("password");
+			address = rsCust.getNString("address");
+			
+			System.out.format("\n%32s%16s\n","","User Login Successful");
+			System.out.format("\n%16s%s%16s%s\n","name = ",name,"email = ",email);
+			
+		}
+		catch(Exception e) {
+			System.out.println("Database ERROR");
+		}		
+		return ;
+		
+	}
+
 	private void signUp() throws IOException {
 		
+		String query,address;
 		System.out.printf("\nEnter Email: ");
 		email = input.readLine().toLowerCase();
-		System.out.printf("\nEnter your Name:");
+		System.out.printf("\nEnter your Name: ");
 		name = input.readLine();
+		System.out.printf("\nEnter Address(Optional): ");
+		address = input.readLine();
+		
 		//database for signup
+		if(address.isEmpty())
+			query = "INSERT INTO customers(Name,Email,password) Values('"+name+"', '"+email+"','"+password+"')";
+		else
+			query = "INSERT INTO customers(Name,Email,password,address) Values('"+name+"', '"+email+"','"+password+"','"+address+"')";
+		
+		if(checkCustUser(email)!=0 | checkEmpUser(email)!= 0) {
+			System.out.println("\n**Email already EXISTS**\n");
+			userInput();
+		}
+		
+		
+		try {	
+			stmt = conn.createStatement();
+				
+			int a = stmt.executeUpdate(query);
+			if(a==1) {
+				conn.commit();
+				isSignedIn = true;
+				System.out.println("User profile created successfully");
+			}
+		}
+		catch(SQLException e) {
+			isSignedIn = false;
+			System.out.println("Database Error: 100x3");
+			System.exit(0);
+		}
 		
 		
 	}
+
+	private int checkCustUser(String email) throws IOException {
+		String query = "SELECT COUNT(*) FROM Customers WHERE email=?";
+		ResultSet rsCust=null;
+		
+		
+		try {
+			//setting prepared statement object and set parameters
+			ps=conn.prepareStatement(query);
+			ps.setString(1, email);
+			
+			//execute query
+			rsCust = ps.executeQuery();
+			rsCust.next();
+			
+			return rsCust.getInt(1);
+		}
+		catch(Exception e) {
+			System.out.println("Database ERROR");
+			userInput();
+		}
+		return 0;
+	}
+	
+	
+	private int checkEmpUser(String email) throws IOException {
+		String query = "SELECT COUNT(*) FROM Employees WHERE email=?";
+		ResultSet rsEmp = null;
+		
+		
+		try {
+			//setting prepared statement object and set parameters
+			ps=conn.prepareStatement(query);
+			ps.setString(1, email);
+			
+			//execute query
+			rsEmp = ps.executeQuery();
+			rsEmp.next();
+			
+			return rsEmp.getInt(1);
+			
+		}
+		catch(Exception e) {
+			System.out.println("Database ERROR");
+			userInput();
+		}
+		return 0;
+		
+	}
+	
+	
 	
 	public String getEmail() {
 		return email;
@@ -60,18 +232,15 @@ public class LogIn {
 	
 	public String getName() {
 		// returns name by retrieving from database or local variable
-		if(name == null) {
-			name = retrieveName();
-		}
 		return name;
 	}
 	
-	//retrieves user name from database
-	private String retrieveName() {
-		
-		
-		
-		return name;
-		
+	public boolean getEmpStatus() {
+		return isEmployee;
 	}
+	
+	public boolean getManagerStatus() {
+		return isManager;
+	}
+ 	
 }
